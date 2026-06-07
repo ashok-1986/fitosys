52c9466eee063d33212c69c502496e4734ea2934
Ashok Verma <verma.86ashok@gmail.com>
2026-06-07 16:57:22 +0530

    feat: integrate GSAP story scroll architecture into landing page

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,21 @@ import { AboutSection } from "@/components/sections/AboutSection";
 import { FaqSection } from "@/components/sections/FaqSection";
 import { CtaBanner } from "@/components/sections/CtaBanner";
 
+import FlowArt, { FlowSection } from "@/components/ui/story-scroll";
+
 export default function LandingPage() {
   return (
-    <>
-      <HeroSection />
-      <PainTicker />
-      <TheProblemSection />
-      <FeaturesSection />
-      <AiSection />
-      <HowItWorksSection />
-      <PricingSection />
-      <TestimonialsSection />
-      <AboutSection />
-      <FaqSection />
-      <CtaBanner />
-    </>
+    <FlowArt>
+      <FlowSection className="bg-[#0A0A0A]"><HeroSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><PainTicker /><TheProblemSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><FeaturesSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><AiSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><HowItWorksSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><PricingSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><TestimonialsSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><AboutSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><FaqSection /></FlowSection>
+      <FlowSection className="bg-[#0A0A0A]"><CtaBanner /></FlowSection>
+    </FlowArt>
   );
 }
